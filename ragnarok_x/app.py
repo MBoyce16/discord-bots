8b5964a24c54bb9641e0887c7d33f558884abc9e
MBoyce <mboyce@cecinnovations.com>
2025-07-20 19:00:39 -0400

    Updated stat calc and added to app interactions

--- ragnarok_x/app.py
+++ ragnarok_x/app.py
@@ -6,9 +6,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import typing as T
-from stat_convert import formula_factory
-from stat_convert.haste import CD_reduc, CT_reduc
-from how_much_more.stat_calc import NeededStat
+from stat_calculation import stat_factory
 
 load_dotenv()
 
@@ -25,59 +23,73 @@ async def help(interaction):
     msg = f"TODO: provide summary of each function"
     await interaction.response.send_message(content=msg, ephemeral=True)
 
-@tree.command(name="Raw to Final", description="Convert raw Crit, ASPD, or Haste to Final%.")
-async def convert_raw(interaction, stat: T.Literal['Crit', 'Haste', 'ASPD'], amount:int):
-    calc = formula_factory(stat)
-    final_val = calc(amount)
-    await interaction.response.send_message(content=f"Final {stat}: {final_val}%", ephemeral=True)
+@tree.command(name="Convert Crit", description="Convert crit to %Final Crit.")
+async def convert_raw(interaction,
+                      amount: T.Union[int, float]):
 
-@tree.command(name="Convert Haste", description="Convert haste and/or final haste to CD and CT reduction.")
-async def convert_haste(interaction, haste: int, final_haste: float):
-    CD_calc = CD_reduc(haste, final_haste)
-    CT_calc = CT_reduc(haste, final_haste)
+    stat_clss = stat_factory('CRIT')
+    out_msg = stat_clss.convert_input(input_type='raw', input_val=amount)
+    await interaction.response.send_message(content=out_msg, ephemeral=True)
 
-    await interaction.response.send_message(content=f"CD Reduction: {CD_calc} secs\nCT Reduction: {CT_calc} secs", ephemeral=True)
+@tree.command(name="Convert ASPD", description="Convert ASPD to %Final ASPD.")
+async def convert_raw(interaction,
+                      amount: T.Union[int, float]):
+
+    stat_clss = stat_factory('ASPD')
+    out_msg = stat_clss.convert_input(input_type='raw', input_val=amount)
+    await interaction.response.send_message(content=out_msg, ephemeral=True)
+
+@tree.command(name="Convert HASTE", description="Convert Haste or %Final Haste to CD Reduction or CT Reduction.")
+async def convert_raw(interaction,
+                      amount: T.Union[int, float],
+                      stat:T.Literal['Haste', '%Final Haste'],
+                      output: ['CT Reduction', 'CD Reduction']):
+
+    input_type = 'raw' if stat == 'Haste' else 'final'
+    stat_clss = stat_factory(output)
+    out_msg = stat_clss.convert_input(input_type=input_type, input_val=amount)
+    await interaction.response.send_message(content=out_msg, ephemeral=True)
 
 @tree.command(name='How much more?', description="Determine how much more stat you need to reach some target goal.")
-async def which_is_better(interaction,
-                          target_stat:T.Literal['Crit', 'ASPD', 'CD Reduction', 'CT Reduction'],
+async def how_much_more(interaction,
+                          target_stat:T.Literal['ASPD', 'CRIT', 'CD Reduction', 'CT Reduction'],
                           target_amount:float,
                           stat_to_evaluate: T.Literal['raw', 'final'],
                           current_raw:int=0,
                           current_final:float=0):
-    needed = NeededStat(stat=target_stat, needed_stat=stat_to_evaluate,
-                        amount_stat_needed=target_amount,
-                        current_raw = 0,
-                        current_final=current_final)
-    await interaction.response.send_message(content=f"Not implemented yet", ephemeral=True)
+
+    stat_clss = stat_factory(target_stat)
+    out_msg = stat_clss.needed_input(current_raw=current_raw,
+                                     current_final=current_final,
+                                     stat_to_quant=stat_to_evaluate,
+                                     target_amt=target_amount)
+
+    await interaction.response.send_message(content=out_msg, ephemeral=True)
+
 
 @tree.command(name='Which is better?', description="Compare Raw vs. %Final stat to see which is better.")
-async def which_is_better(interaction, stat: T.Literal['Crit', 'Haste', 'ASPD'], amount:int):
-    ...
-    await interaction.response.send_message(content=f"Not implemented yet", ephemeral=True)
+async def which_is_better(interaction,
+                          target_stat: T.Literal['ASPD', 'CRIT', 'CD Reduction', 'CT Reduction'],
+                          current_raw: int,
+                          raw_added: int,
+                          final_added: float,
+                          current_final: float):
+
+    stat_clss = stat_factory(target_stat)
+    out_msg = stat_clss.compare_inputs(raw=raw_added,
+                                       final=final_added,
+                                       current_raw=current_raw,
+                                       current_final=current_final)
+
+
+    await interaction.response.send_message(content=out_msg, ephemeral=True)
+
 
 @bot.event
 async def on_ready():
     # Sync commands on command tree with discord.
     await tree.sync()
     print(f"{bot.user} is now up and running.")
-
-
-# @bot.command()
-# async def convert_crit(ctx, crit: int):
-#     fcrit = crit_to_final_crit(crit)
-#     await ctx.send(f"Final Crit: {fcrit}%")
-#
-# @bot.command()
-# async def convert_aspd(ctx, aspd: int):
-#     faspd = aspd_to_final_aspd(aspd)
-#     await ctx.send(f"Final ASDP: {faspd}%")
-#
-# @bot.command()
-# async def convert_haste(ctx, haste: int):
-#     fhaste = haste_to_final_haste(haste)
-#     await ctx.send(f"Final Haste: {fhaste}%")
-
 
 
 if __name__ == "__main__":
---
+++ ragnarok_x/stat_calculation/STAT.py
@@ -0,0 +1,86 @@
+from idlelib.run import Executive
+
+from sympy import symbols, Eq, solve
+import typing as T
+
+class Stat:
+    def __init__(self):
+        self.name = None
+        self.raw_name = None
+        self.final_name = None
+
+        self.raw = symbols('raw', interger=True, positive=True)
+        self.final = symbols('final', integer=False)
+        self.stat = symbols('stat', integer=False, postive=True)
+
+        self.raw_expr = self.raw
+        self.final_expr = self.final
+
+        self.base_eq = Eq(self.raw_expr + self.final_expr, self.stat)
+
+    def _solve(self, inputs:T.Dict[str, T.Union[float, int]]):
+        frmt_inputs = {self._get(k): v for k,v in inputs.items()}
+        eq_to_solve = self.base_eq.subs(frmt_inputs)
+        result = solve(eq_to_solve)[0]
+        return result
+
+    def _get(self, attr:str) -> T.Any:
+        return self.__getattribute__(attr)
+
+    def convert_input(self,
+                      input_type: T.Literal['raw', 'final'],
+                      input_val:T.Union[float, int]) -> str:
+
+        needed_name = self._get(input_type + '_name')
+        raw_val = input_val if input_type == 'raw' else 0
+        final_val = input_val if input_type == 'final' else 0
+
+        input_dict = {'raw': raw_val, 'final': final_val}
+        result = self._solve(input_dict)
+
+        return f'{input_val} {needed_name} provides {final_val} {self.name}'
+
+    def compare_inputs(self,
+                       raw:int,
+                       final:float,
+                       current_raw:int,
+                       current_final:float) -> str:
+
+        current_dict = {'raw': current_raw, 'final': current_final}
+        raw_dict = {'raw': raw + current_raw, 'final': current_final}
+        final_dict = {'raw': current_raw, 'final': final + current_final}
+
+        current_solve = self._solve(current_dict)
+        final_solve = self._solve(raw_dict)
+        raw_solve = self._solve(final_dict)
+
+        return f'Raw stat adds :{raw_solve - current_solve} {self.name},\nFinal stat adds: {final_solve - current_solve} {self.name}'
+
+    def needed_input(self,
+                    current_raw:int,
+                    current_final: float,
+                    stat_to_quant:T.Literal['raw', 'final'],
+                    target_amt: float) -> str:
+
+        needed_name = self._get(stat_to_quant + '_name')
+
+        if stat_to_quant == 'raw':
+            quant_val = current_raw
+            static_val = current_final
+            static_stat = 'final'
+
+        elif stat_to_quant == 'final':
+            quant_val = current_final
+            static_val = current_raw
+            static_stat = 'raw'
+
+        else:
+            raise Exception(f'Stat to quant {stat_to_quant} needs to be "raw" or "final"')
+
+
+        input_dict = {static_stat: static_val, 'stat': target_amt}
+        result = self._solve(input_dict)
+
+        stat_needed = result - quant_val
+
+        return f'To reach {target_amt} {self.name}, you need: {stat_needed} {needed_name}'
