7995c19936870d17663791fc1ddbe0aa91ab1ecf
MBoyce16 <boyce.matthew@hotmail.com>
2025-07-26 11:16:11 -0400

    Saving to transfer workspace

--- ragnarok_x/app.py
+++ ragnarok_x/app.py
@@ -23,40 +23,40 @@ async def help(interaction):
     msg = f"TODO: provide summary of each function"
     await interaction.response.send_message(content=msg, ephemeral=True)
 
-@tree.command(name="Convert Crit", description="Convert crit to %Final Crit.")
-async def convert_raw(interaction,
-                      amount: T.Union[int, float]):
+@tree.command(name="convert_crit", description="Convert crit to %Final Crit.")
+async def convert_crit(interaction,
+                      amount: float):
 
     stat_clss = stat_factory('CRIT')
     out_msg = stat_clss.convert_input(input_type='raw', input_val=amount)
     await interaction.response.send_message(content=out_msg, ephemeral=True)
 
-@tree.command(name="Convert ASPD", description="Convert ASPD to %Final ASPD.")
-async def convert_raw(interaction,
-                      amount: T.Union[int, float]):
+@tree.command(name="convert_aspd", description="Convert ASPD to %Final ASPD.")
+async def convert_asdp(interaction,
+                      amount: float):
 
     stat_clss = stat_factory('ASPD')
     out_msg = stat_clss.convert_input(input_type='raw', input_val=amount)
     await interaction.response.send_message(content=out_msg, ephemeral=True)
 
-@tree.command(name="Convert HASTE", description="Convert Haste or %Final Haste to CD Reduction or CT Reduction.")
-async def convert_raw(interaction,
-                      amount: T.Union[int, float],
+@tree.command(name="convert_haste", description="Convert Haste or %Final Haste to CD Reduction or CT Reduction.")
+async def convert_haste(interaction,
+                      amount: int,
                       stat:T.Literal['Haste', '%Final Haste'],
-                      output: ['CT Reduction', 'CD Reduction']):
+                      output: T.Literal['CT Reduction', 'CD Reduction']):
 
     input_type = 'raw' if stat == 'Haste' else 'final'
     stat_clss = stat_factory(output)
     out_msg = stat_clss.convert_input(input_type=input_type, input_val=amount)
     await interaction.response.send_message(content=out_msg, ephemeral=True)
 
-@tree.command(name='How much more?', description="Determine how much more stat you need to reach some target goal.")
+@tree.command(name='how_much_more', description="Determine how much more stat you need to reach some target goal.")
 async def how_much_more(interaction,
                           target_stat:T.Literal['ASPD', 'CRIT', 'CD Reduction', 'CT Reduction'],
                           target_amount:float,
                           stat_to_evaluate: T.Literal['raw', 'final'],
                           current_raw:int=0,
-                          current_final:float=0):
+                          current_final:float=0.0):
 
     stat_clss = stat_factory(target_stat)
     out_msg = stat_clss.needed_input(current_raw=current_raw,
@@ -67,7 +67,7 @@ async def how_much_more(interaction,
     await interaction.response.send_message(content=out_msg, ephemeral=True)
 
 
-@tree.command(name='Which is better?', description="Compare Raw vs. %Final stat to see which is better.")
+@tree.command(name='which_is_better', description="Compare Raw vs. %Final stat to see which is better.")
 async def which_is_better(interaction,
                           target_stat: T.Literal['ASPD', 'CRIT', 'CD Reduction', 'CT Reduction'],
                           current_raw: int,
